b62d31ec90a030cf39e25b3fc8a9ee31515a4e11
SpaceC00kies <spacecookies1993@gmail.com>
2025-05-28 02:45:37 +0700

    firebase reconfig

--- firebase.ts
+++ firebase.ts
@@ -1,19 +1,20 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
-import { getStorage } from "firebase/storage";
+// firebase.ts
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
+import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA...",
+  apiKey: "AIzaSyAmhIItKW9XR4cgtCxtTUm2EKCY17xNOlo",
   authDomain: "hajobjah.firebaseapp.com",
   projectId: "hajobjah",
-  storageBucket: "hajobjah.appspot.com",
-  messagingSenderId: "XXXXXXXXX",
-  appId: "1:XXXXXXXXX:web:XXXXXXXXX"
+  storageBucket: "hajobjah.firebasestorage.app",
+  messagingSenderId: "441394350866",
+  appId: "1:441394350866:web:7b83583818449c0f3901cb",
+  measurementId: "G-LXKMEJQPCH"
 };
 
 const app = initializeApp(firebaseConfig);
-
 export const db = getFirestore(app);
 export const auth = getAuth(app);
 export const storage = getStorage(app);
